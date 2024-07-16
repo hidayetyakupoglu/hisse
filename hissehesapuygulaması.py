b4a2453b217cb32719ae288935599683b9553260
hidayetyakupoglu <80601785+hidayetyakupoglu@users.noreply.github.com>
2024-07-16 14:51:39 +0300

    Update hissehesapuygulaması.py

--- hissehesapuygulaması.py
+++ hissehesapuygulaması.py
@@ -407,7 +407,7 @@ if hisse_adi:
     st.write(f":blue[**ÖZSERMAYE KARLILIĞINA GÖRE HEDEF FİYAT**]: {ozsermaye_hf:,.2f}")
     st.write(f":chart:**:blue[TÜM HESAPLAMALARIN ORTALAMA FİYATI:]** {ortalama_hesap:,.2f}")
     st.write(f" :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}")    
-
+  """
   elif operation == "BİR ÇEYREK SONRAKİ HEDEF FİYATI TAHMİNİ":  
     c7_y = c7+(c7-c7_2)
     #c7_3 = c7+c6 ## Yılsonu Net Kar Tahmini
@@ -500,6 +500,7 @@ if hisse_adi:
     st.write(f":chart:**:blue[TÜM HESAPLAMALARIN ORTALAMA FİYATI:]** {ortalama_hesap:,.2f}")
     st.write(f" :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}")  
 
+  """
   elif operation == "3. Çeyrek Bilanço Hisse Oranları-9 Aylık":
     c6 = c7/3
     c7_3 = c7+c6 ## Yılsonu Net Kar Tahmini
@@ -524,6 +525,7 @@ if hisse_adi:
     st.write(f":chart:**:blue[TÜM HESAPLAMALARIN ORTALAMA FİYATI:]** {ortalama_hesap:,.2f}")
     st.write(f" :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}")
 
+  """
   elif operation == "4. Çeyrek Bilanço Hisse Oranları-12 Aylık":
     #c7_3 = c7+c6 ## Yılsonu Net Kar Tahmini
     c16_4 = c7 / c4 ## Yılsonu EPS(Hisse Başı Kazanç) Tahmini
@@ -545,7 +547,7 @@ if hisse_adi:
     st.write(f":blue[**ÖZSERMAYE KARLILIĞINA GÖRE HEDEF FİYAT**]: {ozsermaye_hf:,.2f}")
     st.write(f":chart:**:blue[TÜM HESAPLAMALARIN ORTALAMA FİYATI:]** {ortalama_hesap:,.2f}")
     st.write(f" :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}")    
-      
+  """    
   #if operation == "ORTALAMA HEDEF FİYAT":
   #st.write(ortalama_hesap)
   #if ortalama_hesap < kapanıs :
