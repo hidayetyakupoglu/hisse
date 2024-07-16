462dc6a04d99bfc05021fcc4b8b4a18a53707b6d
hidayetyakupoglu <80601785+hidayetyakupoglu@users.noreply.github.com>
2024-07-16 22:57:05 +0300

    Update hissehesapuygulaması.py

--- hissehesapuygulaması.py
+++ hissehesapuygulaması.py
@@ -7,7 +7,8 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+import numpy as np
+import yfinance as yf
 
 def main():
     st.sidebar.title("İletişim Bilgileri")
