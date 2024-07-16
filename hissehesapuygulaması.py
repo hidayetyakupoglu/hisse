83a0e44c10cd103672c993d533268c06b41b92f2
hidayetyakupoglu <80601785+hidayetyakupoglu@users.noreply.github.com>
2024-07-16 21:46:16 +0300

    Add files via upload

--- hissehesapuygulaması.py
+++ hissehesapuygulaması.py
@@ -7,9 +7,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-#import investpy
-import numpy as np
-import yfinance as yf
+
 
 #streamlit.config.theme.base = "dark"
 st.title("**HİSSE FİYAT HESAPLAYICISI**")
